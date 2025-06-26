1398767bc8f2936cd2824a4c700d1406edf853ae
MyeonghoonNam <codenmh0822@naver.com>
2025-06-26 22:12:14 +0900

    feat #3 : 유틸 함수 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,15 @@ export function cn(...inputs: ClassValue[]) {
 export function decodeJWT(token: string) {
   return JSON.parse(atob(token.split('.')[1]));
 }
+
+export function toMatrix<T>(array: T[], columns: number): T[][] {
+  return Array.from({ length: Math.ceil(array.length / columns) }).map((_, rowIndex) =>
+    array.slice(rowIndex * columns, rowIndex * columns + columns)
+  );
+}
+
+export function formatTime(seconds: number): string {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
