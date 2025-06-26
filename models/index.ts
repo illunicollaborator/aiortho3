7f951f23d024ac93f78e0055726c08349dbc576e
MyeonghoonNam <codenmh0822@naver.com>
2025-06-26 22:11:56 +0900

    feat #3 : 엔티티 Model 타입 지정

---
+++ models/index.ts
@@ -0,0 +1,27 @@
+export interface Hospital {
+  hospitalCode: string;
+  name: string;
+  typeCode: string;
+  type: string;
+  regionCode: string;
+  city: string;
+  districtCode: string;
+  district: string;
+  dong: string;
+  zip: string;
+  address: string;
+}
+
+export interface MedicalDepartment {
+  code: string;
+  name: string;
+}
+
+export interface Nurse {
+  id: number;
+  name: string;
+  department: string;
+  license: string;
+  experience?: string;
+  phone: string;
+}
