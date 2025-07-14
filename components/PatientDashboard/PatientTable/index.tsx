516201b0ffcc2d46aab4f51123e9b0a4f03c70ab
MyeonghoonNam <codenmh0822@naver.com>
2025-07-14 15:27:02 +0900

    refactor #7 : 디버그 로직 제거

--- components/PatientDashboard/PatientTable/index.tsx
+++ components/PatientDashboard/PatientTable/index.tsx
@@ -116,7 +116,7 @@ const PatientTable = () => {
 
           {/* 테이블 본문 */}
           <div className="w-full">
-            {true ? (
+            {patients.length === 0 ? (
               <div className="flex flex-col items-center w-full mt-10 xl:mt-15">
                 <p className="text-[var(--aiortho-gray-600)] mb-4 ">
                   ‘환자 등록’ 후 처방이 가능합니다.
