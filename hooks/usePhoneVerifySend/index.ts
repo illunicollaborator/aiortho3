6e8d857cd9709b4e6bc444ac9e6f859062484d81
MyeonghoonNam <codenmh0822@naver.com>
2025-07-16 19:25:46 +0900

    refactor #13 : 휴대폰 인증 전송 훅 타입 구체화

--- hooks/usePhoneVerifySend/index.ts
+++ hooks/usePhoneVerifySend/index.ts
@@ -1,8 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { postPhoneVerifySend } from '@/api/admin/common';
+import { ErrorResponse } from '@/api/types';
+import { PostPhoneVerifyRequest, PostPhoneVerifyResponse } from '@/api/admin/common/types';
 
 const usePhoneVerifySend = () => {
-  return useMutation({
+  return useMutation<PostPhoneVerifyResponse, ErrorResponse, PostPhoneVerifyRequest>({
     mutationFn: postPhoneVerifySend,
   });
 };
