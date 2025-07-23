948a3f03295ad92f8873449b9d1954e378a7f439
MyeonghoonNam <codenmh0822@naver.com>
2025-07-23 14:48:15 +0900

    fix #12 : 변수 타입 수정

--- app/(protected)/prescriptions/patients/[id]/hooks/useActivePrescription/index.ts
+++ app/(protected)/prescriptions/patients/[id]/hooks/useActivePrescription/index.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { getActivePrescription } from '@/api/prescription';
 
-export const useActivePrescription = (patientId: string) => {
+export const useActivePrescription = (patientId: number) => {
   return useQuery({
     queryKey: ['activePrescription', patientId],
     queryFn: () => getActivePrescription({ patientId }),
