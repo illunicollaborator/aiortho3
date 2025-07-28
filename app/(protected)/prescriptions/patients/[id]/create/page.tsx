06905838a512e4e40780a41cb17a76c95cf6089e
MyeonghoonNam <codenmh0822@naver.com>
2025-07-28 15:03:57 +0900

    feat #18 : 치료 프로그램 구성 모달 기능 구현

--- app/(protected)/prescriptions/patients/[id]/create/page.tsx
+++ app/(protected)/prescriptions/patients/[id]/create/page.tsx
@@ -1,14 +1,74 @@
 'use client';
 
 import { useParams } from 'next/navigation';
+import { usePatient } from '../hooks/usePatient';
+import PatientInfoCard from '@/components/PatientInfoCard';
+import { useState } from 'react';
+import { Plus } from 'lucide-react';
+import { ProgramCreateModal } from './components';
+import { useStandardProgram } from './hooks';
+import { GetStandardProgramListResponse } from '@/api/standardProgram/types';
+import PrescriptionProgramCard from '@/components/PrescriptionProgramCard';
 
 export default function CreatePrescriptionPage() {
   const { id } = useParams();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const patientQuery = usePatient(id as string);
+  const standardProgramQuery = useStandardProgram();
+  const [program, setProgram] = useState<GetStandardProgramListResponse['presets'][0]>();
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSelectProgram = (program: GetStandardProgramListResponse['presets'][0]) => {
+    setProgram(program);
+  };
+
+  if (!patientQuery.data || !standardProgramQuery.data) {
+    return null;
+  }
+
+  const { data: patient } = patientQuery;
+  const { data: standardProgram } = standardProgramQuery;
 
   return (
-    <div>
-      <h1>CreatePrescriptionPage</h1>
-      <p>{id}</p>
-    </div>
+    <section className="flex flex-col">
+      <h1 className="text-2xl font-bold text-[var(--aiortho-gray-900)] lg:text-3xl mb-8">
+        처방하기
+      </h1>
+      <PatientInfoCard patient={patient} hidePrescription />
+
+      <h1 className="text-2xl font-bold text-[var(--aiortho-gray-900)] lg:text-3xl mt-22 mb-3">
+        치료 프로그램 구성
+      </h1>
+      <h2 className="text-[var(--aiortho-gray-600)] mb-8">
+        환자에게 처방할 치료 운동 종류를 선택해주세요
+      </h2>
+
+      {patient.prescription ? (
+        <PrescriptionProgramCard prescription={patient.prescription} />
+      ) : (
+        <button
+          type="button"
+          onClick={handleOpenModal}
+          className="flex w-full h-12 px-4 py-3 items-center justify-center gap-2 self-stretch rounded-xl border border-[var(--aiortho-primary)] cursor-pointer mb-3"
+        >
+          <Plus className="w-5 h-5 text-[var(--aiortho-primary)] font-bold" />
+          <span className="text-base text-[var(--aiortho-primary)] font-bold">프로그램 생성</span>
+        </button>
+      )}
+
+      <ProgramCreateModal
+        isOpen={isModalOpen}
+        standardProgram={standardProgram.presets}
+        onClose={handleCloseModal}
+        onSelect={handleSelectProgram}
+      />
+    </section>
   );
 }
