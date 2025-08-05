982b296878988fea4e3493c62993934ef1a59440
MyeonghoonNam <codenmh0822@naver.com>
2025-08-05 09:21:08 +0900

    chore #28 : eslint rule 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: [
-    'next',
-    'next/core-web-vitals',
-    'plugin:prettier/recommended', // Prettier와 충돌 방지
-  ],
+  extends: ['next', 'next/core-web-vitals', 'plugin:prettier/recommended'],
   plugins: ['react', 'react-hooks', 'jsx-a11y'],
   rules: {
-    // 필요 시 커스터마이징
-    'react/react-in-jsx-scope': 'off', // Next.js에서는 필요 없음
-    'prettier/prettier': ['error'], // Prettier 룰을 ESLint로 강제
+    'prettier/prettier': ['error'],
   },
 };
