066e5730eb5ac7d3cb362be04940877bbad10d6c
MyeonghoonNam <codenmh0822@naver.com>
2025-08-06 16:27:22 +0900

    style #28 : 미사용 코드 제거

--- app/(protected)/profile/components/EditProfile/DoctorProfileEditForm/index.tsx
+++ app/(protected)/profile/components/EditProfile/DoctorProfileEditForm/index.tsx
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import OrthoInput from '@/components/OrthoInput';
 import { zodResolver } from '@hookform/resolvers/zod';
