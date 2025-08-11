5b5baeed241fcfa73e766dda6656648c83b33d6e
MyeonghoonNam <codenmh0822@naver.com>
2025-08-11 15:32:14 +0900

    feat : react query default option 추가

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
