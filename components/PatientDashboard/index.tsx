33ab310d07224a6de0e076a253b4107ea326c32d
MyeonghoonNam <codenmh0822@naver.com>
2025-09-16 01:27:55 +0900

    style : 환자 대시보드 조건부 UI 수정

--- components/PatientDashboard/index.tsx
+++ components/PatientDashboard/index.tsx
@@ -2,12 +2,13 @@
 
 import { Button } from '../ui/button';
 import PatientTable from './PatientTable';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useAuthStore } from '@/store/authStore';
 import { isDoctorRole } from '@/lib/utils';
 
 const PatientDashboard = () => {
   const router = useRouter();
+  const pathname = usePathname();
   const { auth } = useAuthStore();
 
   if (!auth) return null;
@@ -16,12 +17,14 @@ const PatientDashboard = () => {
     <section className="flex flex-col gap-6">
       <div className="flex w-full flex-col md:flex-row gap-2 md:gap-0 justify-between md:jitems-center">
         <div className="flex w-full items-center gap-5">
-          <h2 className="text-2xl font-bold text-[var(--aiortho-gray-900)] lg:text-3xl">
+          <h2 className="text-2xl font-bold text-[var(--aiortho-gray-900)] lg:text-[32px]">
             환자 명단
           </h2>
-          <span className="text-[var(--aiortho-gray-600)] text-xl font-medium leading-none lg:text-2xl">
-            {'(최근 3개월 기준)'}
-          </span>
+          {pathname === '/home' && (
+            <span className="text-[var(--aiortho-gray-600)] text-xl font-medium leading-none lg:text-[22px]">
+              {'(최근 3개월 기준)'}
+            </span>
+          )}
         </div>
 
         <Button
